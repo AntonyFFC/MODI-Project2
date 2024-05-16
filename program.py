d98de9e4fc7b78eafaff7ebb2106ee3e18f4eb86
akowals2 <01178230@pw.edu.pl>
2024-05-16 20:00:55 +0200

    punkt pierwszy zrobiony

--- program.py
+++ program.py
@@ -14,3 +14,18 @@ u_ucz, y_ucz = danestatucz[:,0], danestatucz[:,1]
 u_wer, y_wer = danestatwer[:,0], danestatwer[:,1]
 
 # Narysowanie danych statycznych
+plt.figure(figsize=(10,5))
+plt.subplot(1, 2, 1)
+plt.plot(u_ucz, y_ucz, 'b.')
+plt.title('Zbiór uczący')
+plt.xlabel('Sygnał wejściowy (u)')
+plt.xlabel('Sygnał wejściowy (y)')
+
+plt.subplot(1, 2, 2)
+plt.plot(u_wer, y_wer, 'r.')
+plt.title('Zbiór weryfikujący')
+plt.xlabel('Sygnał wejściowy (u)')
+plt.ylabel('Sygnał wyjściowy (y)')
+
+plt.tight_layout()
+plt.show()
