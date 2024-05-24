cdc36dc69db62ecfa200a0baf471afaa0164cf6a
akowals2 <01178230@pw.edu.pl>
2024-05-24 13:41:18 +0200

    Finished point 2 b)

--- zadanie2.py
+++ zadanie2.py
@@ -28,24 +28,99 @@ plt.show()
 
 # b)
 
-def fit_dynamic_model(u, y, nA, nB):
+def create_regression_matrix(u, y, nA, nB):
     N = len(y)
+    M = np.zeros((N, nA + nB))
+    
+    for i in range(nB):
+        M[:, i] = np.concatenate((np.zeros(i + 1), u[:N - i - 1]))
+    
+    for i in range(nA):
+        M[:, nB + i] = np.concatenate((np.zeros(i + 1), y[:N - i - 1]))
+    
+    return M
 
-    # Tworzenie macierzy Toeplitza dla sygnałów u i y
-    U = toeplitz(u, np.zeros(nB))
-    Y = toeplitz(y, np.zeros(nA))
+def fit_model(u, y, nA, nB):
+    M = create_regression_matrix(u, y, nA, nB)
+    M = M[max(nA, nB):]
+    Y = y[max(nA, nB):]
+    W = np.linalg.lstsq(M, Y, rcond=None)[0]
+    return W
 
-    # Przygotowanie macierzy regresji
-    Phi = np.hstack([U[nB:], Y[nA:]])
+def predict_model(u, y, W, nA, nB, recursive=False):
+    N = len(y)
+    y_pred = np.zeros(N)
     
-    # Obcięcie wektora odpowiedzi
-    y = y[max(nA, nB):]
+    for k in range(max(nA, nB), N):
+        if recursive:
+            y_k = y_pred
+        else:
+            y_k = y
+            
+        y_pred[k] = sum(W[i] * u[k - i - 1] for i in range(nB)) + sum(W[nB + i] * y_k[k - i - 1] for i in range(nA))
     
-    # Dopasowanie modelu
-    theta = np.linalg.lstsq(Phi, y, rcond=None)[0]
+    return y_pred
+
+def calculate_error(y_true, y_pred):
+    return np.mean((y_true - y_pred) ** 2)
+
+
+orders = [1, 2, 3]
+results = []
+
+for n in orders:
+    W_ucz = fit_model(u_ucz, y_ucz, nA=n, nB=n)
+    W_wer = fit_model(u_wer, y_wer, nA=n, nB=n)
     
-    # Rozdzielenie współczynników
-    b = theta[:nB]
-    a = theta[nB:]
+    y_pred_ucz_nonrec = predict_model(u_ucz, y_ucz, W_ucz, nA=n, nB=n, recursive=False)
+    y_pred_ucz_rec = predict_model(u_ucz, y_ucz, W_ucz, nA=n, nB=n, recursive=True)
     
-    return b, a
+    y_pred_wer_nonrec = predict_model(u_wer, y_wer, W_wer, nA=n, nB=n, recursive=False)
+    y_pred_wer_rec = predict_model(u_wer, y_wer, W_wer, nA=n, nB=n, recursive=True)
+    
+    error_ucz_nonrec = calculate_error(y_ucz, y_pred_ucz_nonrec)
+    error_ucz_rec = calculate_error(y_ucz, y_pred_ucz_rec)
+    error_wer_nonrec = calculate_error(y_wer, y_pred_wer_nonrec)
+    error_wer_rec = calculate_error(y_wer, y_pred_wer_rec)
+    
+    results.append((n, error_ucz_nonrec, error_ucz_rec, error_wer_nonrec, error_wer_rec))
+    
+    plt.figure(figsize=(12, 8))
+    plt.suptitle(f'Model stopnia {n}', fontsize=15)
+
+    plt.subplot(2, 2, 1)
+    plt.plot(y_ucz, label='Prawdziwe')
+    plt.plot(y_pred_ucz_nonrec, label='Predykcja')
+    plt.title(f'Dane Uczące Brak Rekurencji')
+    plt.legend()
+    
+    plt.subplot(2, 2, 2)
+    plt.plot(y_ucz, label='Prawdziwe')
+    plt.plot(y_pred_ucz_rec, label='Predykcja')
+    plt.title(f'Dane Uczące Rekurencja')
+    plt.legend()
+    
+    plt.subplot(2, 2, 3)
+    plt.plot(y_wer, label='Prawdziwe')
+    plt.plot(y_pred_wer_nonrec, label='Predykcja')
+    plt.title(f'Dane Weryfikujące Brak Rekurencji')
+    plt.legend()
+    
+    plt.subplot(2, 2, 4)
+    plt.plot(y_wer, label='Prawdziwe')
+    plt.plot(y_pred_wer_rec, label='Predykcja')
+    plt.title(f'Dane Weryfikujące Rekurencja')
+    plt.legend()
+    
+    plt.tight_layout()
+    plt.show()
+
+# Wyświetlenie wyników
+import pandas as pd
+
+df = pd.DataFrame(results, columns=['Stopien', 'Błąd Ucz B-Rek', 'Błąd Ucz Rek', 'Błąd Wer B-Rek', 'Błąd Wer Rek'])
+print(df)
+
+# Wybór najlepszego modelu
+best_model = df.loc[df['Błąd Wer Rek'].idxmin()]
+print(f'Najlepszy Model: Stopnia {best_model["Stopien"]} z błędem na danych weryfikujących z rekurencją: {best_model["Błąd Wer Rek"]}')
